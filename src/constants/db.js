06680e71bd2f4ba45b200b7f87974f89b3647b18
Umidjon <umidjongafforov175@gmail.com>
2023-10-21 20:05:49 +0500

    9

--- src/constants/db.js
+++ src/constants/db.js
@@ -1,38 +1,35 @@
+import {img1,img2,img3} from "./домашний"
+import {img4,img5} from "./effko food"
 export function getData() {
 	return [
 		{
 			title: 'JavaScript',
 			price: 17.99,
-			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2F3gf746AKRbWNjB8OCoEB&w=3840&q=75',
+			Image:img1,
 			id: 1,
 		},
 		{
 			title: 'ReactJS',
 			price: 15,
-			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2F65rcPxsLT9ysJDisXF80&w=3840&q=75',
+			Image:img2,
 			id: 2,
 		},
 		{
 			title: 'VueJS',
 			price: 13.5,
-			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2F54vR0DStGoFuZBVkFwWQ&w=3840&q=75',
+			Image:img3,
 			id: 3,
 		},
 		{
 			title: 'NodeJS',
 			price: 13.99,
-			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FmDcwhbguQpyM74jb18M5&w=3840&q=75',
+			Image:img4,
 			id: 4,
 		},
 		{
 			title: 'NestJS',
 			price: 12.5,
-			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FxN7aKkLMQUuD72ikvC1H&w=3840&q=75',
+			Image:img5,
 			id: 5,
 		},
 		{
